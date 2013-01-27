3c844f62f20fca9fea17f283f2a56397cc97169d
Kopachris <Kopachris@49e25ed5-3d46-c005-9b58-ab149935e6ee>
2013-01-27 04:53:54 +0000

    Started bpof branch.  Will work on integrating the volition package and the Blender addon into a single package.

---
+++ branches/bpof/io_scene_pof/__init__.py
@@ -0,0 +1,278 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8 compliant>
+
+bl_info = {
+    "name": "FS2_Open POF format",
+    "author": "Christopher Koch",
+    "blender": (2, 6, 5),
+    "location": "File > Import-Export",
+    "description": "Import-Export POF, Import POF mesh, textures, "
+                   "and helper objects.",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Import-Export"}
+
+if "bpy" in locals():
+    import imp
+    if "import_obj" in locals():
+        imp.reload(import_obj)
+    if "export_obj" in locals():
+        imp.reload(export_obj)
+
+
+import bpy
+from bpy.props import (BoolProperty,
+                       FloatProperty,
+                       StringProperty,
+                       EnumProperty,
+                       )
+from bpy_extras.io_utils import (ExportHelper,
+                                 ImportHelper,
+                                 path_reference_mode,
+                                 )
+
+class ImportPOF(bpy.types.Operator, ImportHelper):
+    """Load a FS2_Open POF File"""
+    bl_idname = "import_scene.pof"
+    bl_label = "Import POF"
+    bl_options = {'PRESET', 'UNDO'}
+
+    filename_ext = ".pof"
+    filter_glob = StringProperty(
+            default="*.pof;*.vp",
+            options={'HIDDEN'},
+            )
+
+##    from_vp = BoolProperty(
+##            name="Import from VP",
+##            description="Import from a VP file - select a VP instead of a POF.",
+##            default=False,
+##            )
+##    pof_name = StringProperty(
+##            name="POF name",
+##            description="Path to the file within the VP.",
+##            )
+    use_smooth_groups = BoolProperty(
+            name="Import smooth groups",
+            description="Try to make smoothgroups using EdgeSplit modifier.",
+            default=False,
+            )   # Probably not very good at it
+    # Helpers:
+    import_center_points = BoolProperty(
+            name="Import center points",
+            description="Import center points as empties.",
+            default=False,
+            )   # Centers are calculated automatically during export
+    import_bound_boxes = BoolProperty(
+            name="Import bounding boxes",
+            description="Import bounding boxes.",
+            default=False,
+            )   # Bounding boxes are calculated automatically during export
+    import_eye_points = BoolProperty(
+            name="Import viewpoints",
+            description="Import eye points as empties.",
+            default=True,
+            )   # Imported as vector empties
+    import_paths = BoolProperty(
+            name="Import paths",
+            description="Import path points as empties.",
+            default=True,
+            )   # Imported as sphere empties parented to first point in path
+    import_gun_points = BoolProperty(
+            name="Import guns",
+            description="Import gun points as empties.",
+            default=True,
+            )   # Imported as vector empties
+    import_mis_points = BoolProperty(
+            name="Import missiles",
+            description="Import missile points as empties.",
+            default=True,
+            )   # Imported as vector empties
+    import_tgun_points = BoolProperty(
+            name="Import gun turrets",
+            description="Import turret gun points as empties.",
+            default=True,
+            )   # Imported as vector empties
+    import_tmis_points = BoolProperty(
+            name="Import missile turrets",
+            description="Import turret missile points as empties.",
+            default=True,
+            )   # Imported as vector empties
+    import_thrusters = BoolProperty(
+            name="Import thrusters",
+            description="Import thrusters as empties.",
+            default=True,
+            )   # Imported as sphere empties
+    import_glow_points = BoolProperty(
+            name="Import glows",
+            description="Import glow points as empties.",
+            default=True,
+            )   # Imported as sphere empties
+##    import_flash_points = BoolProperty(
+##            name="Import muzzleflashes",
+##            description="Import muzzleflash lights as empties.",
+##            default=True,
+##            )   # Imported as regular empties
+    import_special_points = BoolProperty(
+            name="Import special points",
+            description="Import special points as empties.",
+            default=True,
+            )   # Imported as sphere empties
+    import_header_data = BoolProperty(
+            name="Import header",
+            description="Import extra header data as scene custom properties.",
+            default=True,
+            )
+    # Models:
+    import_only_main = BoolProperty(
+            name="Import only hull",
+            description="Import only main LOD and its children.",
+            default=False,
+            )
+    import_detail_levels = BoolProperty(
+            name="Import all LODs",
+            description="Import LOD models.",
+            default=True,
+            )   # Will import each LOD on a separate layer
+    import_detail_boxes = BoolProperty(
+            name="Import detail boxes",
+            description="Import detail box models.",
+            default=True,
+            )
+    import_debris = BoolProperty(
+            name="Import debris",
+            description="Import debris models.",
+            default=True,
+            )   # Will import on a separate layer from LODs.
+    import_turrets = BoolProperty(
+            name="Import turrets",
+            description="Import turret models.",
+            default=True,
+            )   # If LODs selected, will import these parented appropriately
+    import_specials = BoolProperty(
+            name="Import subsystems",
+            description="Import special objects (subsystems).",
+            default=True,
+            )
+##    import_special_debris = BoolProperty(
+##            name="Import subsystem debris",
+##            description="Import special object debris.",
+##            default=True,
+##            )   # If debris selected, will import these on the same layer
+    fore_is_y = BoolProperty(
+            name="Switch axes",
+            description="If true, fore is Blender's +Y-axis.",
+            default=True,
+            )   # Otherwise, fore is Z-axis
+    import_shields = BoolProperty(
+            name="Import shield",
+            description="Import wireframe shield mesh.",
+            default=True,
+            )   # Imports on same layer as highest detail level
+    # Textures:
+    import_textures = BoolProperty(
+            name="Import textures",
+            description="Import textures and UV data.",
+            default=True,
+            )
+    texture_path = StringProperty(
+            name="Texture path",
+            description="Path to search for textures in.",
+            default="../maps/",
+            subtype="FILE_PATH",
+            )
+    texture_format = EnumProperty(
+            name="Texture format",
+            items=(('.dds', "*.dds", "DirectDraw Surface"),
+                   ('.png', "*.png", "Portable Network Graphics"),
+                   ('.tga', "*.tga", "Targa"),
+                   ('.jpg', "*.jpg", "Joint Photographic Experts Group")),
+            default='.dds')
+    pretty_materials = BoolProperty(
+            name="Make materials",
+            description="Make Blender materials from normal, shine, glow maps if possible.",
+            default=False,
+            )
+
+    def execute(self, context):
+        from . import import_pof
+
+        keywords = self.as_keywords(ignore=("filter_glob",))
+        return import_pof.load(self, context, **keywords)
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.prop(self, "use_smooth_groups")
+        layout.prop(self, "import_header_data")
+        layout.prop(self, "fore_is_y")
+
+        box = layout.box()
+        box.label(text="Helpers")
+        box.prop(self, "import_center_points")
+        box.prop(self, "import_bound_boxes")
+        box.prop(self, "import_eye_points")
+        box.prop(self, "import_gun_points")
+        box.prop(self, "import_mis_points")
+        box.prop(self, "import_tgun_points")
+        box.prop(self, "import_tmis_points")
+        box.prop(self, "import_thrusters")
+        box.prop(self, "import_glow_points")
+        box.prop(self, "import_special_points")
+
+        box = layout.box()
+        box.label(text="Models")
+        box.prop(self, "import_only_main")
+        if not self.import_only_main:
+            box.prop(self, "import_detail_levels")
+            box.prop(self, "import_detail_boxes")
+            box.prop(self, "import_debris")
+            box.prop(self, "import_turrets")
+            box.prop(self, "import_specials")
+        box.prop(self, "import_shields")
+
+        box = layout.box()
+        box.label(text="Textures")
+        box.prop(self, "import_textures")
+        if self.import_textures:
+            box.prop(self, "texture_path")
+        box.prop(self, "pretty_materials")
+
+
+def menu_func_import(self, context):
+    self.layout.operator(ImportPOF.bl_idname, text="Parallax Object File (.pof)")
+
+
+def register():
+    bpy.utils.register_module(__name__)
+
+    bpy.types.INFO_MT_file_import.append(menu_func_import)
+    #bpy.types.INFO_MT_file_export.append(menu_func_export)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+    bpy.types.INFO_MT_file_import.remove(menu_func_import)
+   # bpy.types.INFO_MT_file_export.remove(menu_func_export)
+
+if __name__ == "__main__":
+    register()
---
+++ branches/bpof/io_scene_pof/import_pof.py
@@ -0,0 +1,346 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+"""
+This script imports Volition POF files to Blender.
+
+Usage:
+Run this script from "File->Import" menu and then load the desired POF file.
+
+http://code.google.com/p/blender-pof-support/wiki/ImportScript
+"""
+
+
+import os
+import time
+import bpy
+import bmesh
+import mathutils
+from bpy_extras.io_utils import unpack_list, unpack_face_list
+from volition import pof
+
+
+def create_mesh(sobj, use_smooth_groups, fore_is_y, import_textures):
+    """Takes a submodel and adds a Blender mesh."""
+    m = sobj.get_mesh()
+    if use_smooth_groups:
+        m.calculate_sharp_edges()
+
+##    bm = bmesh.new()
+##
+##    for f in m.face_list:
+##        bfverts = list()
+##        for v in f.vert_list:
+##            x = v.co[0]
+##            y = v.co[1]
+##            z = v.co[2]
+##            if fore_is_y:
+##                co = (x, z, y)
+##            else:
+##                co = (x, y, z)
+##            bfverts.append(bm.verts.new(co))
+##        bface = bm.faces.new(bfverts)
+
+    me = bpy.data.meshes.new("{}-mesh".format(sobj.name.decode()))
+##    bm.to_mesh(me)
+
+    vert_list = m.get_vert_list()
+    edge_list, edge_sharps = m.get_edge_list()
+    face_list = m.get_face_list[0]
+    me.from_pydata(vert_list, edge_list, face_list)
+
+    me.validate()
+    me.update()
+
+    if import_textures:
+        uvlayer = me.uv_layers.new()
+        uvlayer = uvlayer.data
+
+    bobj = bpy.data.objects.new("Mesh", me)
+    bobj.name = sobj.name.decode()
+    if use_smooth_groups:
+        bobj.modifiers.new("POF smoothing", "EDGE_SPLIT")
+    return bobj
+
+
+def load(operator, context, filepath,
+        use_smooth_groups=False,
+        import_center_points=False,
+        import_bound_boxes=False,
+        import_eye_points=True,
+        import_paths=True,
+        import_gun_points=True,
+        import_mis_points=True,
+        import_tgun_points=True,
+        import_tmis_points=True,
+        import_thrusters=True,
+        import_glow_points=True,
+        #import_flash_points=True,
+        import_special_points=True,
+        import_header_data=True,
+        import_only_main=False,
+        import_detail_levels=True,
+        import_detail_boxes=True,
+        import_debris=True,
+        import_turrets=True,
+        import_specials=True,
+        #import_special_debris=True,
+        fore_is_y=True,
+        import_shields=True,
+        import_textures=True,
+        texture_path="../maps/",
+        texture_format=".dds",
+        pretty_materials=False
+        ):
+    print("\tloading POF file {}...".format(filepath))
+    filepath = os.fsencode(filepath)
+    cur_time = time.time()
+    pof_file = open(filepath, 'rb')
+    pof_handler = pof.read_pof(pof_file)
+    pof_file.close()
+    new_time = time.time()
+    print("\ttime to load POF handler {} sec".format(new_time - cur_time))
+
+    # we now have a PolyModel instance containing all the chunks
+    # now we check through the kwargs and call the appropriate
+    # methods to read the PolyModel
+
+    if pof_handler.pof_ver >= 2116:
+        pof_hdr = pof_handler.chunks["HDR2"]
+    else:
+        pof_hdr = pof_handler.chunks["OHDR"]
+
+    scene = context.scene
+
+    cur_time = time.time()
+
+    # Textures first
+
+        # we'll do this by making a material for every texture
+        # if we're importing pretty textures, we'll make a
+        # Blender texture each for shine, normal, glow, etc.
+
+        # we'll keep the materials in a list and link every material
+        # to every submodel object.  That way, we can assign
+        # material indices that are the same as the POF texture indices
+
+    if not os.path.isdir(texture_path):
+        print("Given texture path is not a valid directory.")
+        import_textures = False
+
+    if import_textures:
+        txtr_chunk = pof_handler.chunks["TXTR"]
+        # go through txtr_chunk, make Blender images for each
+        # if pretty_textures: get shine, normal, glow, etc., too
+        missing_textures = 0
+        bmats = list()
+        bimgs = list()
+        if pretty_materials:
+            shine_imgs = list()
+            normal_imgs = list()
+            glow_imgs = list()
+        for tex in txtr_chunk.textures:
+            tex_name = tex.decode()
+            tex_path = os.path.join(texture_path, tex_name + texture_format)
+            if os.path.isfile(tex_path):
+                bimgs.append(bpy.data.images.load(tex_path))
+            else:
+                print("Missing texture {}".format(tex_name))
+                missing_textures += 1
+            if pretty_materials:
+                shine_name = tex_name + "-shine" + texture_format
+                norm_name = tex_name + "-normal" + texture_format
+                glow_name = tex_name + "-glow" + texture_format
+                shine_path = os.path.join(texture_path, shine_name)
+                norm_path = os.path.join(texture_path, norm_name)
+                glow_path = os.path.join(texture_path, glow_name)
+                if os.path.isfile(shine_path):
+                    shine_imgs.append(bpy.data.images.load(shine_path))
+                else:
+                    missing_textures += 1
+                if os.path.isfile(norm_path):
+                    normal_imgs.append(bpy.data.images.load(norm_path))
+                else:
+                    missing_textures += 1
+                if os.path.isfile(glow_path):
+                    glow_imgs.append(bpy.data.images.load(glow_path))
+                else:
+                    missing_textures += 1
+        print("Total missing textures: {}".format(missing_textures))
+
+        # Create a material for each texture listed in the chunk
+        # Each object has its own UV layer with the base texture
+        # In addition to the UV layer, the material must have a
+        # texture slot with the image, set to UV coordinates
+        # if using pretty materials, then we need additional
+        # texture slots with the other maps, same UV layer for coords
+        # We do NOT need a material for each object as long as
+        # each object's UV layer has the same name
+
+    # Now submodels
+
+    new_objects = dict()    # dict instead of list so
+                            # we can ref by POF model id
+
+    if import_only_main:
+        # import only first detail level and its children
+
+        # get first detail level
+        hull_id = pof_hdr.sobj_detail_levels[0]
+        hull = pof_handler.submodels[hull_id]
+        hull_obj = create_mesh(hull, use_smooth_groups, fore_is_y, import_textures)
+        new_objects[hull_id] = hull_obj
+
+        # get children
+        child_ids = dict()
+        for model in pof_handler.submodels.values():
+            if model.parent_id == hull_id:
+                child_obj = create_mesh(model, use_smooth_groups, fore_is_y, import_textures)
+                child_obj.parent = hull_obj
+                if fore_is_y:
+                    off_x = model.offset[0]
+                    off_y = model.offset[1]
+                    off_z = model.offset[2]
+                    child_obj.location = (off_x, off_z, off_y)
+                else:
+                    child_obj.location = model.offset
+                new_objects[model.model_id] = child_obj
+
+        # get second children
+        for model in pof_handler.submodels.values():
+            if model.parent_id in child_ids:
+                child_obj = create_mesh(model, use_smooth_groups, fore_is_y, import_textures)
+                child_obj.parent = new_objects[model.parent_id]
+                x_off = pof_handler.submodels[model.parent_id].offset[0] + model.offset[0]
+                y_off = pof_handler.submodels[model.parent_id].offset[1] + model.offset[1]
+                z_off = pof_handler.submodels[model.parent_id].offset[2] + model.offset[2]
+                if fore_is_y:
+                    child_obj.location = (x_off, z_off, y_off)
+                else:
+                    child_obj.location = (x_off, y_off, z_off)
+                new_objects[model.model_id] = child_obj
+
+    else:
+        layer_count = 0
+        debris_layer = None
+        main_detail = None
+
+        if import_detail_levels:
+            for i in pof_hdr.sobj_detail_levels:
+                model = pof_handler.submodels[i]
+                this_obj = create_mesh(model, use_smooth_groups, fore_is_y, import_textures)
+                # put LODs on sep layers from each other
+                this_obj.layers[0] = False
+                this_obj.layers[layer_count] = True
+                layer_count += 1
+                new_objects[model.model_id] = this_obj
+            main_detail = new_objects[0]
+
+        if import_debris:
+            for i in pof_hdr.sobj_debris:
+                model = pof_handler.submodels[i]
+                this_obj = create_mesh(model, use_smooth_groups, fore_is_y, import_textures)
+                # put debris on sep layer from LODs, but same as each other
+                this_obj.layers[0] = False
+                this_obj.layers[layer_count] = True
+                new_objects[model.model_id] = this_obj
+                debris_layer = layer_count
+            layer_count += 1
+
+        if import_turrets:
+            if "TGUN" in pof_handler.chunks:
+                tchunk = pof_handler.chunks["TGUN"]
+                for i in range(len(tchunk.base_sobj)):
+                    model = pof_handler.submodels[tchunk.base_sobj[i]]
+                    this_obj = create_mesh(model, use_smooth_groups, fore_is_y, import_textures)
+                    if main_detail is not None:
+                        this_obj.parent = main_detail
+                    off_x = model.offset[0]
+                    off_y = model.offset[1]
+                    off_z = model.offset[2]
+                    if fore_is_y:
+                        this_obj.location = (off_x, off_z, off_y)
+                    else:
+                        this_obj.location = (off_x, off_y, off_z)
+
+                    if tchunk.barrel_sobj[i] > -1:
+                        bar_model = pof_handler.submodels[tchunk.barrel_sobj[i]]
+                        barrel_obj = create_mesh(bar_model, use_smooth_groups, fore_is_y, import_textures)
+                        barrel_obj.parent = this_obj
+                        off_x += model.offset[0]
+                        off_y += model.offset[1]
+                        off_z += model.offset[2]
+                        if fore_is_y:
+                            barrel_obj.location = (off_x, off_z, off_y)
+                        else:
+                            barrel_obj.location = (off_x, off_y, off_z)
+                        new_objects[bar_model.model_id] = barrel_obj
+                    new_objects[model.model_id] = this_obj
+
+            if "TMIS" in pof_handler.chunks:
+                tchunk = pof_handler.chunks["TMIS"]
+                for i in range(len(tchunk.base_sobj)):
+                    model = pof_handler.submodels[tchunk.base_sobj[i]]
+                    this_obj = create_mesh(model, use_smooth_groups, fore_is_y, import_textures)
+                    if main_detail is not None:
+                        this_obj.parent = main_detail
+                    off_x = model.offset[0]
+                    off_y = model.offset[1]
+                    off_z = model.offset[2]
+                    if fore_is_y:
+                        this_obj.location = (off_x, off_z, off_y)
+                    else:
+                        this_obj.location = (off_x, off_y, off_z)
+
+                    if tchunk.barrel_sobj[i] > -1:
+                        bar_model = pof_handler.submodels[tchunk.barrel_sobj[i]]
+                        barrel_obj = create_mesh(bar_model, use_smooth_groups, fore_is_y, import_textures)
+                        barrel_obj.parent = this_obj
+                        off_x += model.offset[0]
+                        off_y += model.offset[1]
+                        off_z += model.offset[2]
+                        if fore_is_y:
+                            barrel_obj.location = (off_x, off_z, off_y)
+                        else:
+                            barrel_obj.location = (off_x, off_y, off_z)
+                        new_objects[bar_model.model_id] = barrel_obj
+                    new_objects[model.model_id] = this_obj
+
+        if import_specials:
+            for model in pof_handler.submodels.values():
+                if b"subsystem" in model.properties:
+                    this_obj = create_mesh(model, use_smooth_groups, fore_is_y, import_textures)
+                    this_obj.parent = new_objects[model.parent_id]
+                    new_objects[model.model_id] = this_object
+
+    if import_shields and "SHLD" in pof_handler.chunks:
+        model = pof_handler.chunks["SHLD"]
+        this_obj = create_mesh(model, False, fore_is_y, None)
+        this_obj.draw_type = "WIRE"
+        new_objects["shield"] = this_obj
+
+    # done
+
+    for obj in new_objects.values():
+        scene.objects.link(obj)
+
+    scene.update()
+    new_time = time.time()
+    print("\ttime to add objects {}".format(new_time - cur_time))
+
+    return {'FINISHED'}
